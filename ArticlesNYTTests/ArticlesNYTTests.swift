997f2f562e3bedefa039ce73856a1f3a5a07d6ac
Rafael Aviles Puebla <rafaelavilesp@icloud.com>
2026-04-13 11:50:02 -0600

    Initial Commit

---
+++ ArticlesNYTTests/ArticlesNYTTests.swift
@@ -0,0 +1,19 @@
+//
+//  ArticlesNYTTests.swift
+//  ArticlesNYTTests
+//
+//  Created by Rafael Aviles Puebla on 13/04/26.
+//
+
+import Testing
+@testable import ArticlesNYT
+
+struct ArticlesNYTTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
